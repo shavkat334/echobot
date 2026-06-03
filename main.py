50b68589dac3a94f9d90cc14edab21a9a7146f55
djumanov <djumanovdev@gmail.com>
2026-06-03 17:10:03 +0500

    feat: created settings

---
+++ main.py
@@ -0,0 +1,4 @@
+from settings import settings
+
+
+print(settings.TOKEN)
---
+++ settings.py
@@ -0,0 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class Settings:
+    TOKEN = os.getenv("TOKEN")
+
+
+settings = Settings()
